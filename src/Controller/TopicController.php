c6cc2d96f6b5185492fc1a834f169c6199bd2bb5
Denisi64 <simonpro.denis@gmail.com>
2024-12-22 21:50:13 +0100

    commit v5

--- src/Controller/TopicController.php
+++ src/Controller/TopicController.php
@@ -1,16 +1,75 @@
 <?php
-// src/Controller/TopicController.php
-
 namespace App\Controller;
 
+use App\Entity\Topic;
+use App\Form\TopicType;
+use App\Repository\ReplyRepository;
 use App\Repository\TopicRepository;
-use App\Repository\ReplyRepository; // Le repository des réponses (Reply)
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
-class TopicController extends AbstractController
+#[Route('/topic')]
+final class TopicController extends AbstractController
 {
+    #[Route(name: 'app_topic_index', methods: ['GET'])]
+    public function index(TopicRepository $topicRepository): Response
+    {
+        return $this->render('topic/index.html.twig', [
+            'topics' => $topicRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_topic_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $topic = new Topic();
+        $form = $this->createForm(TopicType::class, $topic);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($topic);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_topic_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('topic/new.html.twig', [
+            'topic' => $topic,
+            'form' => $form,
+        ]);
+    }
+    #[Route('/{id}/edit', name: 'app_topic_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Topic $topic, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(TopicType::class, $topic);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_topic_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('topic/edit.html.twig', [
+            'topic' => $topic,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_topic_delete', methods: ['POST'])]
+    public function delete(Request $request, Topic $topic, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $topic->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($topic);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_topic_index', [], Response::HTTP_SEE_OTHER);
+    }
+
     #[Route('/topic/{id}', name: 'app_topic_show')]
     public function show(int $id, TopicRepository $topicRepository, ReplyRepository $replyRepository): Response
     {
@@ -31,4 +90,3 @@ class TopicController extends AbstractController
         ]);
     }
 }
-
